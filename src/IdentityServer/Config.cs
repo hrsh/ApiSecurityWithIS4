a94be3ac94303b7189350b37b83db63c07ccbc84
hrshojaie <hrshojaie@gmail.com>
2021-03-15 19:43:53 +0330

    clean up

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,4 +1,7 @@
-﻿using IdentityServer4;
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -6,40 +9,18 @@ namespace IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<ApiScope> Scopes =>
-            new List<ApiScope>
-            {
-                new ApiScope("api1", "My Api"),
-                new ApiScope("webapp", "My web app")
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
             };
 
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
         public static IEnumerable<Client> Clients =>
-            new List<Client>
-            {
-                new Client
-                {
-                    ClientId = "client",
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes = { "api1" }
-                },
-                new Client
-                {
-                    ClientId = "mvc",
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "api1"
-                    },
-                    AllowOfflineAccess = true,
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    RedirectUris = { "https://localhost:5001/signin-oidc" },
-                    
-                }
-            };
+            new Client[] 
+            { };
     }
 }
--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -1,3 +1,7 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,30 +11,50 @@ namespace IdentityServer
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+        public IWebHostEnvironment Environment { get; }
 
-            services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                .AddInMemoryApiScopes(Config.Scopes)
-                .AddInMemoryClients(Config.Clients);
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
-            if (env.IsDevelopment())
+            // uncomment, if you want to add an MVC-based UI
+            //services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            // uncomment if you want to add MVC
+            //app.UseStaticFiles();
+            //app.UseRouting();
+            
             app.UseIdentityServer();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
+            // uncomment, if you want to add MVC
+            //app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //});
         }
     }
 }
